585f49d9c977d9caa49b10b91daba6bc9b3dedaa
naveenIT <naveen.fullstack22@gmail.com>
2025-11-30 00:02:57 +0530

    #1 - db config.

--- backend/Selectify/settings.py
+++ backend/Selectify/settings.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173/",
+    "http://localhost:5173",
 ]
 
 ROOT_URLCONF = 'Selectify.urls'
@@ -69,8 +69,12 @@ WSGI_APPLICATION = 'Selectify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testDB',
+        'USER': 'postgres',
+        'PASSWORD': 'R&B@18f06v3r@1807',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
