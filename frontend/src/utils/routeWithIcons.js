2cbade63411e95982d11fd3d656aaadf537e02aa
naveenIT <naveen.fullstack22@gmail.com>
2026-01-07 23:34:30 +0530

    #1 - initial setup completed.

--- frontend/src/utils/routeWithIcons.js
+++ frontend/src/utils/routeWithIcons.js
@@ -3,7 +3,6 @@ import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import InfoIcon from '@mui/icons-material/Info';
 import { About } from '../components/About';
 import { Profile } from '../components/Profile';
-import Home from '../components/Home';
 import LogoutIcon from '@mui/icons-material/Logout';
 import AdminDashboard from '../components/AdminDashboard';
 import Home1 from '../components/Home1';
