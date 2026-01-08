9f042b8032890d94cbb010df13152e75d24aed60
naveenIT <naveen.fullstack22@gmail.com>
2026-01-08 20:34:16 +0530

    database config changes updated.

--- backend/Selectify/settings.py
+++ backend/Selectify/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import environ
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -88,8 +89,13 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+DATABASE_URL = env.str("DATABASE_URL")
+
 DATABASES = {
-    'default': env.db()
+    'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
 }
 
 
