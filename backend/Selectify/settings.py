49d61a523cd24264c0140db0e35d1373465d4013
naveenIT <naveen.fullstack22@gmail.com>
2026-01-08 09:12:55 +0530

    #1 - deployment files

--- backend/Selectify/settings.py
+++ backend/Selectify/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = [BACKEND_URL]
+ALLOWED_HOSTS = env.list("BACKEND_URL")
 
 
 # Application definition
